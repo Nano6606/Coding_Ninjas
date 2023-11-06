54e5430c5039d362f9f7be2a15b87f73baa21e43
Nano6606 <129884507+Nano6606@users.noreply.github.com>
2023-11-06 22:16:56 +0530

    Create if-else (Decision Making).cpp

---
+++ if-else (Decision Making).cpp
@@ -0,0 +1,14 @@
+Link of the Question:-
+https://www.codingninjas.com/studio/problems/if-else-decision-making_8357235?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+
+SOLUTION:-
+  
+string compareIfElse(int a, int b) {
+    if (a < b) {
+        return "smaller";
+    } else if (a > b) {
+        return "greater";
+    } else {
+        return "equal";
+    }
+}
