4bb30c24910ef99bbb8bdc55fe12e26809ee39fd
Nano6606 <129884507+Nano6606@users.noreply.github.com>
2024-01-27 10:45:11 +0530

    Sum Of First N Numbers.cpp

---
+++ Sum Of First N Numbers.cpp
@@ -0,0 +1,11 @@
+QUESTION LINK:-
+https://www.codingninjas.com/studio/problems/sum-of-first-n-numbers_8876068?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+
+
+SOLUTION:-
+
+#include <iostream>
+long long sumFirstN(long long n) {
+    // Calculate the sum using the formula
+    return (n * (n + 1)) / 2;
+}
